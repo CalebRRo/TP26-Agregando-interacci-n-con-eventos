7c4408e0c5a2aacf2771164ace61572dc098428a
CalebRR <yohannagraciano@gmail.com>
2022-11-06 13:54:11 -0300

    TP 26 interacciones con eventos

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+    let imputTitulo = document.getElementById("titulo");
+    let estadoSecreto = 0;
+
+    titulo.addEventListener("mouseover",()=>{
+         titulo.style.color = "#7f7f7f"
+    })
+
+    const checkKey = (key) => {
+        switch (true) {
+            case key === "s" && estadoSecreto === 0:
+                ++estadoSecreto;   
+                break;
+                case key === "e" && estadoSecreto === 1:
+                ++estadoSecreto;   
+                break;
+                case key === "c" && estadoSecreto === 2:
+                ++estadoSecreto;   
+                break;
+                case key === "r" && estadoSecreto === 3:
+                ++estadoSecreto;   
+                break;
+                case key === "e" && estadoSecreto === 4:
+                ++estadoSecreto;   
+                break;
+                case key === "t" && estadoSecreto === 5:
+                ++estadoSecreto;   
+                break;
+                case key === "o" && estadoSecreto === 6:
+                ++estadoSecreto
+                alert("Secreto Magico")
+                break;
+        
+            default:
+                estadoSecreto = 0;
+                break;
+        }
+    }
+
+    imputTitulo.addEventListener("keyup", (e) => {
+        checkKey(e.key)
+    })
+
+}
